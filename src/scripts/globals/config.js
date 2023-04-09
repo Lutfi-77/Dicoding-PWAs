fac89dc825d2a8b12c4c52a7057a6127aa76f957
Lutfi-77 <xenonoffic@gmail.com>
2023-04-09 18:40:11 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: 'c67d750d553f2a633ba6465ef442abb8',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
